5c235955e3dd403c2e570cfedc5d7b612f50ca65
Aaron Kurtzhals <aaron.kurtzhals@gmail.com>
2012-07-02 21:29:32 -0500

    Started test_join_table

--- TestWhistTable.rb
+++ TestWhistTable.rb
@@ -2,8 +2,17 @@ require "WhistTable"
 require "test/unit"
  
 class TestWhistTable < Test::Unit::TestCase
-  def test_constructor
-    new_table = WhistTable.new
-    assert_instance_of(WhistTable, new_table)
+  
+  def setup
+    @new_table = WhistTable.new
   end
+  
+  def test_constructor
+    assert_instance_of(WhistTable, @new_table)
+  end
+  
+  def test_join_table
+    team1 = Team.new
+  end
+  
 end
