ccc5c6f5a42bbab4245e12568d536c62f0e65a81
Aaron Kurtzhals <aaron.kurtzhals@gmail.com>
2012-07-05 20:00:54 -0500

    Added TestTeam

---
+++ TestTeam.rb
@@ -0,0 +1,7 @@
+class TestWhistTable < Test::Unit::TestCase
+  
+  def test_constructor
+    assert_instance_of(Team, Team.new)
+  end
+  
+end
--- test_suite.rb
+++ test_suite.rb
@@ -1,1 +1,2 @@
 require 'TestWhistTable'
+require 'TestTeam'
