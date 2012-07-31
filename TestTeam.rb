f61706aa3e1a80daa9d07b1effa00adff377690d
Aaron Kurtzhals <aaron.kurtzhals@gmail.com>
2012-07-30 20:23:32 -0500

    corrected TeamTest setup

--- TestTeam.rb
+++ TestTeam.rb
@@ -3,11 +3,15 @@ require "Team"
 class TestTeam < Test::Unit::TestCase
   
   def setup
-    @new_player = Player.new
+    @new_team = Team.new
   end
   
   def test_constructor
-    assert_instance_of(Team, Team.new)
+    assert_instance_of(Team, @new_team)
+  end
+  
+  def test_add_player
+    
   end
   
 end
