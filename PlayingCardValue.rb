572cda29f8a24f0b319ca13499d22f3f492d2e90
Aaron Kurtzhals <aaron.kurtzhals@gmail.com>
2012-08-05 16:16:16 -0500

    Added 2-Queen values

--- PlayingCardValue.rb
+++ PlayingCardValue.rb
@@ -6,6 +6,17 @@ class PlayingCardValue
 
   PlayingCardValue::ACE = PlayingCardValue.new('Ace')
   PlayingCardValue::KING = PlayingCardValue.new('King')
+  PlayingCardValue::QUEEN = PlayingCardValue.new('Queen')
+  PlayingCardValue::JACK = PlayingCardValue.new('Jack')
+  PlayingCardValue::TEN = PlayingCardValue.new('10')
+  PlayingCardValue::NINE = PlayingCardValue.new('9')
+  PlayingCardValue::EIGHT = PlayingCardValue.new('8')
+  PlayingCardValue::SEVEN = PlayingCardValue.new('7')
+  PlayingCardValue::SIX = PlayingCardValue.new('6')
+  PlayingCardValue::FIVE = PlayingCardValue.new('5')
+  PlayingCardValue::FOUR = PlayingCardValue.new('4')
+  PlayingCardValue::THREE = PlayingCardValue.new('3')
+  PlayingCardValue::TWO = PlayingCardValue.new('2')
 
   private_class_method :new
   
--- TestPlayingCardValue.rb
+++ TestPlayingCardValue.rb
@@ -3,6 +3,17 @@ class TestPlayingCardValue < Test::Unit::TestCase
   def test_values
     assert_instance_of(PlayingCardValue, PlayingCardValue::ACE)
     assert_instance_of(PlayingCardValue, PlayingCardValue::KING)
+    assert_instance_of(PlayingCardValue, PlayingCardValue::QUEEN)
+    assert_instance_of(PlayingCardValue, PlayingCardValue::JACK)
+    assert_instance_of(PlayingCardValue, PlayingCardValue::TEN)
+    assert_instance_of(PlayingCardValue, PlayingCardValue::NINE)
+    assert_instance_of(PlayingCardValue, PlayingCardValue::EIGHT)
+    assert_instance_of(PlayingCardValue, PlayingCardValue::SEVEN)
+    assert_instance_of(PlayingCardValue, PlayingCardValue::SIX)
+    assert_instance_of(PlayingCardValue, PlayingCardValue::FIVE)
+    assert_instance_of(PlayingCardValue, PlayingCardValue::FOUR)
+    assert_instance_of(PlayingCardValue, PlayingCardValue::THREE)
+    assert_instance_of(PlayingCardValue, PlayingCardValue::TWO)
   end
   
   def test_private_constructor
