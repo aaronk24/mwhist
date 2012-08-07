fb3c8eccf03dfcf07921ba6bf985a1fcfaef4843
Aaron Kurtzhals <aaron.kurtzhals@gmail.com>
2012-08-06 21:01:38 -0500

    Added BLACK and private constructor

--- PlayingCardColor.rb
+++ PlayingCardColor.rb
@@ -1,3 +1,6 @@
 class PlayingCardColor
   PlayingCardColor::RED = PlayingCardColor.new
+  PlayingCardColor::BLACK = PlayingCardColor.new
+  
+  private_class_method :new
 end
--- TestPlayingCardColor.rb
+++ TestPlayingCardColor.rb
@@ -1,6 +1,17 @@
 require 'PlayingCardColor'
 class TestPlayingCardColor < Test::Unit::TestCase
+  
   def test_colors
     assert_instance_of(PlayingCardColor, PlayingCardColor::RED)
+    assert_instance_of(PlayingCardColor, PlayingCardColor::BLACK)
   end
+  
+  def test_private_constructor
+    begin
+      disallowedValue = PlayingCardColor.new
+    rescue
+    end
+    assert_nil(disallowedValue)
+  end
+  
 end
