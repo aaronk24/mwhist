ede82b7adde7e584e00c8b40d2ff94a069952c44
Aaron Kurtzhals <aaron.kurtzhals@gmail.com>
2012-08-11 12:30:54 -0500

    Added deal_random_card

--- PlayingCardDeck.rb
+++ PlayingCardDeck.rb
@@ -1,2 +1,17 @@
+require 'PlayingCard'
+require 'PlayingCardSuit'
+require 'PlayingCardValue'
 class PlayingCardDeck
+  
+  def initialize
+    @cards_dealt = 0
+  end
+  
+  def deal_random_card
+    unless @cards_dealt >= 52
+      @cards_dealt += 1
+      PlayingCard.new(PlayingCardValue::SEVEN, PlayingCardSuit::CLUBS)
+    end
+  end
+  
 end
--- TestPlayingCardDeck.rb
+++ TestPlayingCardDeck.rb
@@ -3,4 +3,20 @@ class TestPlayingCardDeck < Test::Unit::TestCase
   def test_constructor
     assert_instance_of(PlayingCardDeck, PlayingCardDeck.new)
   end
+  
+  def test_card_deal
+    assert_instance_of(PlayingCard, PlayingCardDeck.new.deal_random_card)
+  end
+  
+  def test_deck_deal
+    myDeck = PlayingCardDeck.new
+    nbrCardsDealt = 0 #stops what could otherwise be an infinite loop
+    cardsDealt = Array.new
+    while cardDealt = myDeck.deal_random_card and nbrCardsDealt < 60
+      cardsDealt << cardDealt
+      nbrCardsDealt += 1 
+    end
+    assert_equal(52, cardsDealt.size)
+  end
+  
 end
